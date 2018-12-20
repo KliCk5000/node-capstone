7639f8ca7f40cbd5fbb77efea8fd61f348622b47
Nick Dean <klick5000@gmail.com>
2018-12-19 21:18:18 -0700

    add test, travis, and heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(express.static("public"));
 app.listen(process.env.PORT || 8080);
+
+app.use(morgan('common'));
+
+module.exports = { app };
---
+++ test/test-server.js
@@ -0,0 +1,21 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const faker = require("faker");
+const mongoose = require("mongoose");
+
+const { app } = require("../server");
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe("Server status", function() {
+  it("should return 200 status", function() {
+    return chai
+      .request(app)
+      .get("/")
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
+  });
+});
